1222130da36900898ee83a3756d5b720dac342a2
ikegami5 <important2nd@gmail.com>
2016-09-10 15:17:50 +0900

    fix php

--- hello.php
+++ hello.php
@@ -1,25 +1,26 @@
 <?php
 
 //変数
-$message = "Hello from PHP!";
+$message = "Hello from PHP!\n";
 echo $message;
 
 //演算
 $int1 = 1;
 $int2 = 2;
 $int3 = 3;
-echo $int1 + $int2;
+echo ($int1 + $int2)."\n";
 
 //型を表示
 var_dump($message);
+echo "\n";
 
 //定数
 define("MY_FINAL_VALUE", "14");
-echo MY_FINAL_VALUE;
+echo MY_FINAL_VALUE."\n";
 
 //特殊定数(行数)
 var_dump(__LINE__);
-
+echo "\n";ß
 
 
 ?>
