d874ac7514dac0b76feecb959203a20a6a7b47ab
ikegami5 <important2nd@gmail.com>
2016-09-18 21:20:19 +0900

    fix

--- hello.php
+++ hello.php
@@ -48,14 +48,14 @@ if (1 and true and "hello") {
 }
 
 //三項演算子
-$seven = 7
-$five = 5
+$seven = 7;
+$five = 5;
 $max = ($seven > $five) ? $seven : $five
 //it means ...
 if ($seven > $five) {
-	$max = $seven
+	$max = $seven;
 } else {
-	$max = $five
+	$max = $five;
 }
 echo $max;
 
