b0263ed7ef39f1db807d2920c92570e1b8797fb8
ikegami5 <important2nd@gmail.com>
2016-09-20 00:23:23 +0900

    fix form

--- form.php
+++ form.php
@@ -1,5 +1,14 @@
 <?php
 
+$username = "";
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+	$username = $_POST["username"];
+	$error = false;
+	if (strlen($username) > 8) {
+		$error = true;
+	}
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -9,9 +18,10 @@
 	<link rel="stylesheet" type="text/css" href="./style.css" />
 </head>
 <body>
-	<form>
-		<input type="text" name="username" placeholder="user name" />
+	<form action="#" method="POST">
+		<input type="text" name="username" placeholder="user name" value="<?php echo htmlspecialchars($username, ENT_QUOTES, "utf-8"); ?>" />
 		<input type="submit" value="Check!" />
 	</form>
+	<p><?php if ($error) { echo "too long<br>"; } ?></p>
 </body>
 </html>
