25dc195ab69cd78364b122008ccd42043dff20ff
ikegami5 <important2nd@gmail.com>
2016-09-21 19:37:58 +0900

    add calculate

--- hello.js
+++ hello.js
@@ -3,3 +3,19 @@ console.log("Hello from script!");
 //変数
 var hello = "Hello", x = 3;
 console.log(hello);
+
+var y = 10;
+var z = y % x;
+console.log("10 % 3 = ");
+console.log(z);
+z = y / x;
+console.log("10 / 3 = ");
+console.log(z);
+x += 3;
+console.log("3 + 3 = ");
+console.log(x);
+x--;
+console.log("6 - 1 = ")
+console.log(x);
+
+
