fa2ad49938ecd4bde74a4a2b635896b6ec16e62e
ikegami5 <important2nd@gmail.com>
2016-11-19 22:49:01 +0900

    fix

--- cgi-bin/pythonTest.py
+++ cgi-bin/pythonTest.py
@@ -4,7 +4,6 @@ import sys
 
 import io
 
-
 def main():
 	br = "</br>"
 	print("Content-type: text/html\n")
@@ -14,11 +13,12 @@ def main():
 		<head>
 			<meta charset="UTF-8" />
 			<title>Python練習1</title>
-			<link rel="stylesheet" type="text/css" href="./style.css" />
+			<link rel="stylesheet" type="text/css" href="../html/style.css" />
 		</head>
 		<body>
 			<h1>Python練習1</h1>
 			<p>{0}</p>
+			<p>{1}</p>
 			<h2>Link</h2>
 			<a href="./index.html">TOP</a>
 		</body>
@@ -28,7 +28,8 @@ def main():
 	version = list(sys.version_info)[0:3]
 	body += "version: {0[0]}.{0[1]}.{0[2]}".format(version)
 	body += br
-	print(html.format(body))
+	debug = sys.stdout.encoding
+	print(html.format(body, debug))
 
 if __name__ == "__main__":
 
