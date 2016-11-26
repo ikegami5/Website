6d1c43f445d33062457e4495297aeedb75b701f2
ikegami5 <important2nd@gmail.com>
2016-11-27 05:11:08 +0900

    add mathquiz

---
+++ cgi-bin/httpHandler.py
@@ -0,0 +1,28 @@
+#!/usr/local/bin/python3
+# coding: utf-8
+
+class Response(object):
+	def __init__(self, title, body):
+		self.html = """
+			<!DOCTYPE html>
+			<html lang="ja">
+			<head>
+				<meta charset="UTF-8" />
+				<title>{title}</title>
+				<link rel="stylesheet" type="text/css" href="/style.css" />
+			</head>
+			<body>
+				<h1>{title}</h1>
+				<p>{body}</p>
+				<h2>Link</h2>
+				<a href="/index.html">TOP</a>
+			</body>
+			</html>
+		"""
+		self.title = title
+		self.body = body
+		self.html = self.html.format(title=self.title, body=self.body)
+
+	def respond(self):
+		print("Content-type: text/html\n")
+		print(self.html)
---
+++ cgi-bin/mathQuiz.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python3
+# coding: utf-8
+from httpHandler import Response
+
+def main():
+	title = "title"
+	body = "test"
+	res = Response(title, body)
+	res.respond()
+
+if __name__ == "__main__":
+	main()
