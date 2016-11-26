ae06f34560ecba963f9b8a745af5cb363d9314a7
ikegami5 <important2nd@gmail.com>
2016-11-27 07:21:59 +0900

    link test

--- cgi-bin/link.py
+++ cgi-bin/link.py
@@ -5,8 +5,7 @@ from httpHandler import Response
 def main():
 	title = "title"
 	body = """
-		Link test<br>
-		<a href="/cgi-bin/link.py">Link</a>
+		Link<br>
 	"""
 	res = Response(title, body)
 	res.respond()
