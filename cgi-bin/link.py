38aeb01734fc51861c0f0e4d24bc55f92ee08a44
ikegami5 <important2nd@gmail.com>
2016-11-28 04:01:41 +0900

    fix

--- cgi-bin/link.py
+++ cgi-bin/link.py
@@ -9,7 +9,7 @@ def main():
 	body = """
 		Your data:
 	""" + br
-	body += "<div id=indent>" + sys.stdin.read() + br + "<div />"
+	body += '<div class="indent">' + sys.stdin.read() + br + "<div />"
 	res = Response(title, body)
 	res.respond()
 
