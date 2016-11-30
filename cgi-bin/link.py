4b8f1f20e28b0348aea372d0a7c631284440dc2d
ikegami5 <important2nd@gmail.com>
2016-12-01 03:53:08 +0900

    dbtest

--- cgi-bin/link.py
+++ cgi-bin/link.py
@@ -1,9 +1,18 @@
 #!/usr/local/bin/python3
 # coding: utf-8
 from httpHandler import Response, Request
+import MySQLdb
 from dbPass import dbName, dbPass
 
 def main():
+	dbConnector = MySQLdb.connect(
+			user = dbName,
+			passwd = dbPass,
+			host = "localhost",
+			db = "mathQuizUsers"
+		)
+	dbCursor = dbConnector.cursor()
+
 	request = Request()
 	data = request.data
 	title = "Log in success!"
@@ -14,13 +23,23 @@ def main():
 	elif "password" not in data:
 		print("Location: /cgi-bin/mathQuiz.py?error=true\n")
 	else:
+		dbData = ""
+		dbCursor.execute("SELECT * FROM users")
+		for row in dbCursor.fetchall():
+			dbData += str(row)
+
 		body += """
 			Your name: {name} {br}
 			Your password: {password} {br}
 		""".format(name = data["name"].value, password = data["password"].value, br = br)
-		body += dbName + ": " + dbPass + br
+
+		body += dbdata + br
+
 		res = Response(title, body)
 		res.respond()
+
+	dbCursor.close
+	dbConnector.close
 
 if __name__ == "__main__":
 	main()
