679a9dc05a8607ab074731543630d8625696d795
ikegami5 <important2nd@gmail.com>
2016-12-02 12:45:56 +0900

    fix

--- cgi-bin/mathQuiz.py
+++ cgi-bin/mathQuiz.py
@@ -7,10 +7,10 @@ def main():
 	title = "Math Quiz"
 	body = """
 		<form method="get" action="/cgi-bin/signUp.py?error=none>
-			<button type="button" value="Sign up" />
+			<button type="submit" value="signUp" name="action">Sign up</button>
 		</form>
 		<form method="get" action="/cgi-bin/signIn.py?error=none>
-			<button type="button" value="Sign in" />
+			<button type="submit" value="signIn" name="action">Sign in</button>
 		</form>
 	"""
 	res = Response(title, body)
