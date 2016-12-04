72caa5d9f95ab0eb0308b26dddcfc96cfa2cc181
ikegami5 <important2nd@gmail.com>
2016-12-05 08:33:49 +0900

    signin prepare

---
+++ cgi-bin/signIn.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3
+# coding: utf-8
+from httpHandler import Response, Request
+
+def main():
+	br = "<br />"
+	title = "Sign In"
+	body = ""
+
+if __name__ == '__main__':
+	main()
