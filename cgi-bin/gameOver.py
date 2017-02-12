36e39cbd5ad772332e4a14c34d7452bb638c8c8d
ikegami5 <important2nd@gmail.com>
2017-02-12 23:11:07 +0900

    score renew when get higher

--- cgi-bin/gameOver.py
+++ cgi-bin/gameOver.py
@@ -9,38 +9,46 @@ def main():
 	br = "<br />"
 	title = "Game Over"
 
-	dbConnector = MySQLdb.connect(
-		user = dbName,
-		passwd = dbPass,
-		host = "localhost",
-		db = "mathQuizUsers"
-	)
-	dbCursor = dbConnector.cursor()
+	try:
+		dbConnector = MySQLdb.connect(
+			user = dbName,
+			passwd = dbPass,
+			host = "localhost",
+			db = "mathQuizUsers"
+		)
+		dbCursor = dbConnector.cursor()
 
-	request = Request()
-	score = int(request.data["score"].value)
-	answer = request.data["answer"].value
-	rightAnswer = request.data["rightAnswer"].value
-	if rightAnswer == answer:
-		score = score + 1
-	name = request.data["name"].value
+		request = Request()
+		score = int(request.data["score"].value)
+		answer = request.data["answer"].value
+		rightAnswer = request.data["rightAnswer"].value
+		if rightAnswer == answer:
+			score = score + 1
+		name = request.data["name"].value
 
-	dbCursor.execute("""
-		UPDATE users SET score = {score} WHERE name = "{name}"
-	""".format(name = name, score = str(score)))
-	dbConnector.commit()
+		dbCursor.execute("""
+			SELECT * FROM users WHERE name = "{name}"
+		""".format(name = name))
+		if score > dbCursor.fetchone()[0][2]:
+			dbCursor.execute("""
+				UPDATE users SET score = {score} WHERE name = "{name}"
+			""".format(name = name, score = str(score)))
+			dbConnector.commit()
 
-	dbData = DBExpression("Name", "Score")
-	dbCursor.execute("SELECT * FROM users")
-	for row in dbCursor.fetchall():
-		dbData.appendData(row[0], str(row[2]))
+		dbData = DBExpression("Name", "Score")
+		dbCursor.execute("SELECT * FROM users")
+		for row in dbCursor.fetchall():
+			dbData.appendData(row[0], str(row[2]))
 
-	body = """
-		score = {score}{br}
-	""".format(br = br, score = str(score))
-	body += str(dbData) + br
-	res = Response(title, body)
-	res.respond()
+		body = """
+			score = {score}{br}
+		""".format(br = br, score = str(score))
+		body += str(dbData) + br
+		res = Response(title, body)
+		res.respond()
+	finally:
+		dbCursor.close()
+		dbConnector.close()
 
 if __name__ == '__main__':
 	main()
