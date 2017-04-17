fc21267b05aeef98261986892a88f27d81af9893
ikegami5 <important2nd@gmail.com>
2017-04-18 00:11:39 +0900

    make canvas

--- html/game/main.js
+++ html/game/main.js
@@ -1,6 +1,24 @@
+var CANVAS_WIDTH = 800;
+var CANVAS_HEIGHT = 600;
+
 window.addEventListener('load', init);
 
+var canvas;
+var renderingContext;
 
 function init() {
-	
+	canvas = document.getElementById('mainCanvas');
+	renderingContext = canvas.getContext('2d');
+	canvas.width = CANVAS_WIDTH;
+	canvas.height = CANVAS_HEIGHT;
+	requestAnimationFrame(update);
+}
+
+function update() {
+	requestAnimationFrame(update);
+	render();
+}
+
+function render() {
+	renderingContext.clearRect(0, 0, canvas.width, canvas.height);
 }
