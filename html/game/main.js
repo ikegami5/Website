a58792d8d293f2b9bace7cbd14a5e259341b3214
ikegami5 <important2nd@gmail.com>
2017-04-18 01:34:12 +0900

    add background

--- html/game/main.js
+++ html/game/main.js
@@ -6,12 +6,45 @@ window.addEventListener('load', init);
 var canvas;
 var renderingContext;
 
+var Asset = {
+	assets: [
+		{type: 'image', name: 'background', src: '/game/assets/background.png'}
+	],
+	images: {},
+
+	loadAssets: function(onComplete) {
+		var total = Asset.assets.length;
+		var loadCount = 0;
+		var onLoad = function() {
+			loadCount++;
+			if (loadCount >= total) {
+				onComplete();
+			}
+		};
+		this.assets.forEach(function(asset) {
+			if (asset.type === 'image') {
+				this._loadImage(asset, onLoad);
+			}
+		});
+	},
+
+	_loadImage: function(asset, onLoad) {
+		var image = new Image();
+		image.src = asset.src;
+		image.onload = onLoad;
+		this.images[asset.name] = image;
+	}
+
+};
+
 function init() {
 	canvas = document.getElementById('mainCanvas');
 	renderingContext = canvas.getContext('2d');
 	canvas.width = CANVAS_WIDTH;
 	canvas.height = CANVAS_HEIGHT;
-	requestAnimationFrame(update);
+	Asset.loadAssets(function() {
+		requestAnimationFrame(update);
+	});
 }
 
 function update() {
